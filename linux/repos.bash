f743dc21646dc57eed9ada301aa8e7fbfc3fd5b2
chuck <charlesrymal@gmail.com>
2014-11-15 11:33:26 -0800

    auto

--- linux/repos.bash
+++ linux/repos.bash
@@ -3,7 +3,6 @@ cd
 cd git
 
 cd chuck1.github.io
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
@@ -14,42 +13,36 @@ git status
 cd ../..
 
 cd python
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
 cd ..
 
 cd pywiki
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
 cd ..
 
 cd thesis
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
 cd ..
 
 cd wiki_private
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
 cd ..
 
 cd wiki_public
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
 cd ..
 
 cd wiki_school
-git status
 git add --all
 git commit -m 'auto'
 git push origin master
