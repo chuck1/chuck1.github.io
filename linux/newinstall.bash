3c55531a0357f334a92f9e767a9af4d641a78caa
chuck <charlesrymal@gmail.com>
2014-11-15 11:28:25 -0800

    auto

--- linux/newinstall.bash
+++ linux/newinstall.bash
@@ -4,16 +4,27 @@ cd
 
 apt-get build-dep glfw
 
-apt-get install g++ doxygen graphviz
+apt-get install g++ doxygen graphviz 
 
 apt-get install libxi-dev libxcursor-dev libglew-dev libpng12-dev libfreetype6-dev
 
 apt-get install libboost-system1.55-dev libboost-thread1.55-dev libboost-serialization1.55-dev
 
+apt-get install apache2 php5
+
+# apache
+
+a2enmod cgi
+apache2 restart
+
+cd git/chuck1/github.io/linux
+
+cat apache2.conf >> /etc/apache2/apache2.conf
+
 # startup scripts
 
-cp git/chuck1.github.io/linux/vimrc ~/.vimrc
-cat git/chuck1.github.io/linux/bashrc >> ~/.bashrc
+cp vimrc ~/.vimrc
+cat bashrc >> ~/.bashrc
 
 # git repos
 
@@ -23,6 +34,7 @@ git clone git clone http://github.com/chuck1/python python
 git clone git clone http://github.com/chuck1/pywiki
 git clone git clone http://github.com/chuck1/wiki_private
 git clone git clone http://github.com/chuck1/wiki_subshot
+git clone git clone http://github.com/chuck1/thesis
 
 mkdir nebula-engine
 cd nebula-engine
---
+++ linux/repos.bash
@@ -0,0 +1,61 @@
+
+cd
+cd git
+
+cd chuck1.github.io
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+cd nebula-engine/nebula
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+cd python
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+cd pywiki
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+cd thesis
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+cd wiki_private
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+cd wiki_public
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+cd wiki_school
+git status
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ..
+
+
