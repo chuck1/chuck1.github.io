d0e9733361eac1e5d5fb47afbef81aeb776772cb
chuck <charlesrymal@gmail.com>
2014-12-02 13:27:26 -0800

    auto

--- linux/repos.bash
+++ linux/repos.bash
@@ -37,19 +37,7 @@ git commit -m 'auto'
 git push origin master
 cd ..
 
-cd wiki_private
-git add --all
-git commit -m 'auto'
-git push origin master
-cd ..
-
-cd wiki_public
-git add --all
-git commit -m 'auto'
-git push origin master
-cd ..
-
-cd wiki_school
+cd wiki
 git add --all
 git commit -m 'auto'
 git push origin master
