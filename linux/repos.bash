cedb264c90cfd69c3a80a81b90006f57670c9771
chuck <charlesrymal@gmail.com>
2015-02-11 11:17:46 -0800

    add python_build_system

--- linux/repos.bash
+++ linux/repos.bash
@@ -4,6 +4,7 @@ git_process.bash "git/chuck1/chuck1.github.io"
 git_process.bash "git/chuck1/c-testing"
 git_process.bash "git/chuck1/n-body"
 git_process.bash "git/chuck1/python"
+git_process.bash "git/chuck1/python_build_system"
 git_process.bash "git/thesis"
 git_process.bash "git/wiki_private"
 
